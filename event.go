f9b5d12dede2e59d1d076e0acc26a69ba9975294
Elliott Maguire <glotchimo@pm.me>
2023-07-15 13:01:57 -0700

    Add some inline documentation

--- event.go
+++ event.go
@@ -2,13 +2,23 @@ package gleam
 
 import "fmt"
 
+// Event abstracts a single event produced by a Handler after its defined
+// trigger is detected by the IRC listener.
 type Event struct {
-	Type     string
-	UserID   string
+	// Type is an arbitrary string to describe the type of event.
+	Type string
+
+	// UserID is the ID of the user that issued the event.
+	UserID string
+
+	// Username is the username of the user that issued the event.
 	Username string
-	Data     any
+
+	// Data is any additional data that needs to be attached to the event.
+	Data any
 }
 
+// String prints a human-friendly representation of the event.
 func (e Event) String() string {
 	return fmt.Sprintf("%s from %s (%s) - %v", e.Type, e.UserID, e.Username, e.Data)
 }
--- handler.go
+++ handler.go
@@ -2,6 +2,9 @@ package gleam
 
 import "github.com/gempir/go-twitch-irc/v4"
 
+// Message is a facade type over twitch.PrivateMessage so that users don't
+// need to explicitly import the go-twitch-irc package when adding handlers.
 type Message twitch.PrivateMessage
 
+// Handler is a function that takes an IRC message and produces an event.
 type Handler func(Message) Event
