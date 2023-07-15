e0e4068adee55ec77e128b74c15b92d955ef4c6e
Elliott Maguire <glotchimo@pm.me>
2023-07-15 11:48:31 -0700

    Add Event.String method

--- event.go
+++ event.go
@@ -1,8 +1,14 @@
 package gleam
 
+import "fmt"
+
 type Event struct {
 	Type     string
 	UserID   string
 	Username string
 	Data     any
+}
+
+func (e Event) String() string {
+	return fmt.Sprintf("%s from %s (%s) - %v", e.Type, e.UserID, e.Username, e.Data)
 }
